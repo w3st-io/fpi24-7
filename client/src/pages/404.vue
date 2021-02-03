e0c5a907f53dc996e6c9c06dc2828167b657bb20
aleem-ahmed <opall.io2020@gmail.com>
2021-02-03 16:49:57 -0500

    Small 404 redirect bug fix

--- client/src/pages/404.vue
+++ client/src/pages/404.vue
@@ -4,11 +4,11 @@
 			<h1 class="text-center text-primary">404 Not Found</h1>
 			<p>Strange..</p>
 
-			<RouterLink to="/" class="m-auto text-center">
+			<a href="/" class="m-auto text-center">
 				<BButton variant="primary" class="m-auto text-center">
 					Return Home
 				</BButton>
-			</RouterLink>
+			</a>
 		</BCard>
 	</BContainer>
 </template>
