064c69b58b5181c5dc4d7e7a6018816e4f5c3c4d
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-02-17 13:45:36 -0500

    Improved:

--- client/src/components/SocialMediaPlug.vue
+++ client/src/components/SocialMediaPlug.vue
@@ -1,14 +1,14 @@
 <template>
 	<div class="text-center">
-		<a :href="defaultData.facebookLink" class="text-light">
+		<a :href="defaultData.facebookLink" :class="`text-${variant}`">
 			<FacebookIcon :size="size" class="mr-3" />
 		</a>
 
-		<a :href="defaultData.instagramLink" class="text-light">
+		<a :href="defaultData.instagramLink" :class="`text-${variant}`">
 			<InstagramIcon :size="size" class="mr-3" />
 		</a>
 
-		<a :href="defaultData.youtubeLink" class="text-light">
+		<a :href="defaultData.youtubeLink" :class="`text-${variant}`">
 			<YoutubeIcon :size="size" class="" />
 		</a>
 	</div>
@@ -19,14 +19,19 @@
 	import { FacebookIcon, InstagramIcon, YoutubeIcon } from 'vue-feather-icons'
 
 	// [IMPORT] Personal //
-	import defaultData from '../defaults/companyInfo'
+	import defaultData from '@/defaults/companyInfo'
 
 	export default {
 		props: {
 			size: {
 				type: String,
 				default: "24",
-			}
+			},
+
+			variant: {
+				type: String,
+				default: 'light'
+			},
 		},
 
 		components: {
