46a0a914aa9fe0990d6407a3460658ddf55fc1bf
aleem-ahmed <opall.io2020@gmail.com>
2021-02-18 20:31:25 -0500

    Add Location to Footer

--- client/src/components/nav/Footer.vue
+++ client/src/components/nav/Footer.vue
@@ -2,7 +2,7 @@
 	<div class="w-100 bg-dark text-light">
 		<BContainer>
 			<BRow class="pt-4">
-				<BCol cols="12" class="text-center text-light">
+				<BCol cols="12" class="text-center text-light" data-aos="fade">
 					<h5>
 						{{ companyInfo.companyName }} {{ new Date().getFullYear() }}
 					</h5>
@@ -11,7 +11,26 @@
 					<hr>
 				</BCol>
 
-				<BCol cols="12" sm="4" class="text-center">
+				<BCol cols="12" sm="3" class="text-center text-sm-left" data-aos="fade-up">
+					<h4 class="text-light">Location</h4>
+					<a :href="companyInfo.googleMapsLink" class="mb-3">
+						<h5 class="mb-4">{{ companyInfo.address }}</h5>
+					</a>
+
+					<h4 class="text-light">Hours</h4>
+					<ul class="list-unstyled text-small text-light">
+						<li
+							v-for="(hoo, index) in companyInfo.hoursOfOperation"
+							:key="index"
+						>
+							<span class="text-primary">{{ hoo.days }}:</span>
+							{{ hoo.hours }}
+						</li>
+					</ul>
+					<hr>
+				</BCol>
+
+				<BCol cols="12" sm="3" class="text-center" data-aos="fade-up">
 					<h4>Services</h4>
 					<ul class="list-unstyled text-small">
 						<li>
@@ -30,9 +49,10 @@
 							</RouterLink>
 						</li>
 					</ul>
+					<hr>
 				</BCol>
 
-				<BCol cols="12" sm="4" class="text-center">
+				<BCol cols="12" sm="3" class="text-center" data-aos="fade-up">
 					<h4>Company</h4>
 					<ul class="list-unstyled text-small">
 						<li>
@@ -51,9 +71,10 @@
 							</RouterLink>
 						</li>
 					</ul>
+					<hr>
 				</BCol>
 
-				<BCol cols="12" sm="4" class="text-center">
+				<BCol cols="12" sm="3" class="text-center" data-aos="fade-up">
 					<h4>Careers</h4>
 					<ul class="list-unstyled text-small">
 						<li>
@@ -67,6 +88,7 @@
 							</RouterLink>
 						</li>
 					</ul>
+					<hr>
 				</BCol>
 
 				<BCol cols="12" class="my-3 text-center text-secondary">
