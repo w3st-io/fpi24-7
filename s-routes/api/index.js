55bbea347dac72532e187332b978541358ee1729
aleem-ahmed <opall.io2020@gmail.com>
2021-02-28 01:16:46 -0500

    s

--- s-routes/api/index.js
+++ s-routes/api/index.js
@@ -1,11 +1,12 @@
 // [REQUIRE] //
 const cors = require('cors')
 const express = require('express')
+const HTMLToPDF = require('html-pdf-node')
 
 
 // [REQUIRE] Personal //
 const config = require('../../s-config')
-
+const reports = require('../../s-reports')
 
 // [EXPRESS + USE] //
 const router = express.Router().use(cors())
@@ -21,6 +22,30 @@ router.get(
 			node_env: config.NODE_ENV,
 			__dirname: __dirname
 		})
+	}
+)
+
+
+// [MAIN-ROUTE] //
+router.get(
+	'/generate-report',
+	async (req, res) => {
+		try {
+			const options = { format: 'A4' }
+			const file = { content: reports.standardReport }
+	
+			// [CONVERT] //
+			HTMLToPDF.generatePdf(file, options).then(pdfBuffer => {
+				res.send(pdfBuffer)
+			})
+		}
+		catch (err) {
+			res.send({
+				executed: false,
+				status: false,
+				message: `Error --> ${err}`
+			})
+		}
 	}
 )
 
