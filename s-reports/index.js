e23356c32c2c8b8d06fb9c8b6a11da86752ddab5
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-03-01 13:51:59 -0500

    Improved

--- s-reports/index.js
+++ s-reports/index.js
@@ -1,68 +1,63 @@
 // [EXPORT] //
 module.exports = {
-	standardReport: `
-		<html>
-			<head>
-				<title>Document</title>
-				<link
-					rel="stylesheet"
-					href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-					integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-					crossorigin="anonymous"
-				>
-			</head>
-
-			<body>
-				<div class="container">
-					<div class="row">
-						<div class="col-12">
-							<h1 class="text-center">24/7 Fire Protection Inc Report</h1>
-							<h2>Standard Report</h2>
-						</div>
-					</div>
-
-					<div class="row">
-						<div class="col-6">
-							sdf
-						</div>
-					</div>
-				</div>
-			</body>
-		</html>
-	`,
-
-	standardReport2: (test) => {
+	fireSprinklerInspectionReport: (test) => {
 		return `
-			<html>
-				<head>
-					<title>Document</title>
-					<link
-						rel="stylesheet"
-						href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
-						integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
-						crossorigin="anonymous"
-					>
-				</head>
+		<link
+		rel="stylesheet"
+		href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
+		integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z"
+		crossorigin="anonymous"
+	>
+	<style>
+		@media print { .page { page-break-after: always; } }
+	</style>
+	
+	<div class="container page">
+		<div class="row mb-3">
+			<div class="col-12">
+				<h4 class="font-weight-bold">Fire Sprinkler Inspection Report</h4>
+			</div>
+		</div>
+	
+		<div class="row mb-3">
+			<div class="col-4">
+				<h5 class="font-weight-bold">Subject Property</h5>
+				<h6>123 main st, teaneck, nj</h6>
+			</div>
+		</div>
 		
-				<body>
-					<div class="container">
-						${test}
-						<div class="row">
-							<div class="col-12">
-								<h1 class="text-center">24/7 Fire Protection Inc Report</h1>
-								<h2>Standard Report</h2>
-							</div>
-						</div>
-		
-						<div class="row">
-							<div class="col-6">
-								sdf
-							</div>
-						</div>
-					</div>
-				</body>
-			</html>
+		<div class="row mb-3">
+			<div class="col-12">
+				<h5 class="font-weight-bold">Inspection Conducted By</h5>
+			</div>
+			<div class="col-4">
+				<img src="https://www.24-7fireprotection.com/img/logo.441b8a86.png">
+			</div>
+			<div class="col-4">
+				<h5>24/7 Fire Protection Inc.</h5>
+				<h6>123 main st, teaneck, nj</h6>
+	
+				<h6>Phone: 123 123 1234</h6>
+				<h6>Fax: 123 123 1234</h6>
+			</div>
+		</div>
+	</div>
+	
+	<div class="container page">
+		<div class="row">
+			<div class="col-12">
+				<h6 class="text-center">24/7 Fire Protection Inc Report</h6>
+			</div>	
+		</div>
+	</div>
 		`
 	},
-		
+
+	footerDefault: `
+		<html>
+			<h1 class="text-center">
+				<span class="pageNumber"><span> of <span class="totalPages"><span>
+			</h1>
+		</html>
+	`
 }
--- s-routes/api/index.js
+++ s-routes/api/index.js
@@ -2,6 +2,7 @@
 const cors = require('cors')
 const express = require('express')
 const HTMLToPDF = require('html-pdf-node')
+const pdf = require('html-pdf')
 
 
 // [REQUIRE] Personal //
@@ -26,13 +27,25 @@ router.get(
 )
 
 
-// [MAIN-ROUTE] //
+// [MAIN-ROUTE] html-pdf-node //
 router.get(
 	'/generate-report',
 	async (req, res) => {
 		try {
-			const options = { format: 'A4' }
-			const file = { content: reports.standardReport2('SDFSDFSDF') }
+			const file = { content: reports.fireSprinklerInspectionReport('x'), }
+			const options = {
+				format: 'A4',
+				margin: {
+					top: '.6in',
+					right: '.6in',
+					bottom: '.6in',
+					left: '.6in'
+				},
+				displayHeaderFooter: true,
+				footerTemplate: reports.footerDefault,
+				printBackground: true,
+				preferCSSPageSize: true,
+			}
 	
 			// [CONVERT] //
 			HTMLToPDF.generatePdf(file, options).then(pdfBuffer => {
@@ -48,6 +61,40 @@ router.get(
 		}
 	}
 )
+
+
+// [MAIN-ROUTE] html-pdf //
+router.get(
+	'/generate-report2',
+	async (req, res) => {
+		try {
+			const html = reports.fireSprinklerInspectionReport('x')
+			
+			const options = {
+				format: 'Letter',
+				border: {
+					top: '.6in',
+					right: '.6in',
+					bottom: '.6in',
+					left: '.6in'
+				},
+			}
+	
+			// [CONVERT] //
+			pdf.create(html, options).toBuffer(
+				function (err, buffer) { res.send(buffer) }
+			)
+		}
+		catch (err) {
+			res.send({
+				executed: false,
+				status: false,
+				message: `Error --> ${err}`
+			})
+		}
+	}
+)
+
 
 // [EXPORT] //
 module.exports = router
