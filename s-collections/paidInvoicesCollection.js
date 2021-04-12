d60de3da8dc3ac3c8be32f8fe867f5eb68918978
aleem-ahmed <aleem.ahmed2072@gmail.com>
2021-04-12 01:55:30 -0400

    Improved

--- s-collections/paidInvoicesCollection.js
+++ s-collections/paidInvoicesCollection.js
@@ -1,5 +1,6 @@
 // [REQUIRE] //
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 
 // [REQUIRE] Personal //
@@ -10,8 +11,24 @@ const PaidInvoiceModel = require('../s-models/PaidInvoiceModel')
 // [CREATE] //
 const c_create = async ({ invoiceNumber, stripe_charge_id }) => {
 	try {
-		
-	
+		// [VALIDATE] invoiceNumber //
+		if (!validator.isAscii(invoiceNumber)) {
+			return {
+				executed: true,
+				status: false,
+				message: `paidInvoicesCollection: Invalid invoiceNumber`,
+			}
+		}
+
+		// [VALIDATE] invoiceNumber //
+		if (!validator.isAscii(stripe_charge_id)) {
+			return {
+				executed: true,
+				status: false,
+				message: `paidInvoicesCollection: Invalid stripe_charge_id`,
+			}
+		}
+
 		// [SAVE] //
 		const PaidInvoice = await new PaidInvoiceModel({
 			_id: mongoose.Types.ObjectId(),
